5fb77b6c9910c7f39d9eb515ce6e3afa68698e22
ferry setiawan <ferrys557@gmail.com>
2024-02-22 11:44:03 +0700

    logo ke tiga

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -37,7 +37,7 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <a href="index.html"><img class="logo" width="150px" src="{{ asset('larasGarden/image/lo.png') }}"></a>
+      <a href="index.html"><img class="logo" width="150px" src="{{ asset('larasGarden/image/logo.svg') }}"></a>
 
       <nav id="navbar" class="navbar">
         <ul>
