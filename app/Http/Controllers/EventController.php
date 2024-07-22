0ca9a73aacfd38c2a2727dd4d5494ffaf74994e9
ferrysetiawan <ferrys557@gmail.com>
2024-07-22 11:54:50 +0700

    update event controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,11 +7,22 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    public function __construct()
+    {
+        $this->countEvent = Event::where('tanggal', '>=', Carbon::now())->count();
+        $this->countNews  = Post::count();
+        $this->recentPost = Post::publish()->latest()->take(10)->get();
+    }
+
     public function detail($slug)
     {
+        $countEvent = $this->countEvent;
+        $countNews  = $this->countNews;
         $event = Event::where('slug', $slug)->first();
         return view('event.detail',[
             'event' => $event,
+            'countEvent' => $countEvent,
+            'countNews' => $countNews,
         ]);
     }
 }
