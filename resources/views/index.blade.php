04778aeb9fbac963a50cad2adf8f2ea4481a82e0
ferrysetiawan <ferrys557@gmail.com>
2024-12-19 09:19:57 +0700

    update headline v4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -62,6 +62,24 @@
                                 </div>
                             </div>
                         </div>
+                        @elseif ($key == 2)
+                        <div class="col-sm-6 p-rl-1 p-b-2">
+                            <div class="bg-img1 size-a-5 how1 pos-relative" style="background-image: url({{ asset($item->thumbnail) }});">
+                                <a href="{{ route('news.show', $item->slug) }}" class="dis-block how1-child1 trans-03"></a>
+
+                                <div class="flex-col-e-s s-full p-rl-25 p-tb-20">
+                                    <a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
+                                        Life Style
+                                    </a>
+
+                                    <h3 class="how1-child2 m-t-14">
+                                        <a href="{{ route('news.show', $item->slug) }}" class="how-txt1 size-h-1 f1-m-1 cl0 hov-cl10 trans-03">
+                                            {{ $item->judul }}
+                                        </a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
                         @else
                         <div class="col-sm-6 p-rl-1 p-b-2">
                             <div class="bg-img1 size-a-5 how1 pos-relative" style="background-image: url({{ asset($item->thumbnail) }});">
